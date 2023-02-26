7864c1e6687454e569a71a6ca7cfb14e871a7624
Gabriel Ibenye <gabrielibenye@gmail.com>
2023-02-26 19:35:34 +0100

    bug fix

--- controllers/authController.js
+++ controllers/authController.js
@@ -38,6 +38,7 @@ const createSendToken = (user, statusCode, res) => {
 
 exports.register = catchAsync(async (req, res, next) => {
 	const newUser = await User.create({
+		name: req.body.name,
 		email: req.body.email,
 		password: req.body.password,
 		passwordConfirm: req.body.passwordConfirm,
