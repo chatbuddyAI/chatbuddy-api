61f733e78b67a4aa8660c215db6b7102ad3908e9
Gabriel Ibenye <gabrielibenye@gmail.com>
2023-05-25 05:31:34 +0100

    staging email setup

--- utils/email.js
+++ utils/email.js
@@ -12,10 +12,23 @@ module.exports = class Email {
 
 	newTransport() {
 		if (process.env.NODE_ENV === 'production') {
-			// prod email
+			// prod emails
 			return true;
 		}
 
+		if (process.env.NODE_ENV === 'staging') {
+			return nodemailer.createTransport({
+				host: process.env.MAIL_HOST,
+				port: process.env.MAIL_PORT,
+				secure: true,
+				auth: {
+					user: process.env.MAIL_USERNAME,
+					pass: process.env.MAIL_PASSWORD,
+				},
+			});
+		}
+
+		// dev / local
 		return nodemailer.createTransport({
 			host: process.env.MAIL_HOST,
 			port: process.env.MAIL_PORT,
