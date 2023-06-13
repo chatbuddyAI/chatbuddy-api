b9451f5b4f0573f3b3a988d214f2c976794a705c
Gabriel Ibenye <gabrielibenye@gmail.com>
2023-06-13 12:27:26 +0100

    disabled sending cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -25,17 +25,17 @@ const createSingedToken = (id) =>
 
 const createSendToken = catchAsync(async (user, statusCode, res) => {
 	const token = createSingedToken(user._id);
-	const cookieOptions = {
-		expires: new Date(
-			Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
-		),
-		httpOnly: true,
-	};
+	// const cookieOptions = {
+	// 	expires: new Date(
+	// 		Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+	// 	),
+	// 	httpOnly: true,
+	// };
 	const decoded = await promisify(jwt.verify)(token, process.env.JWT_SECRET);
 
-	if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+	// if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
 
-	res.cookie('jwt', token, cookieOptions);
+	// res.cookie('jwt', token, cookieOptions);
 
 	// Remove password from output
 	user.password = undefined;
